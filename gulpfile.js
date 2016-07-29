7f484b0a0a1eace59b60c33d8d5e6b955bded379
Sanjeev Prajapati <spr@codebee.dk>
2016-07-29 15:38:51 +0545

    enhanced

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,54 @@ var gulp = require('gulp');
 var gulpless = require('gulp-less');
 var gulpkss = require('gulp-kss');
 var gulpconcat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
 
+var onError = function (err) {  
+    console.log(err);
+    this.emit('end');
+};
+
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         }
+//     });
+// });
 
 gulp.task('kss', function() {
     // Generate styleguide with templates 
     return gulp.src(['styles/**/*.less'])
+    .pipe(plumber({
+        errorHandler: onError
+    }))
     .pipe(gulpkss({
     	overview: __dirname + '/styles/styleguide.md',
     	templateDirectory:  __dirname + '/template/'
     }))
-    .pipe(gulp.dest('styleguide/'));
+    .pipe(gulp.dest('styleguide/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('render', function() {
      // Concat and compile all your styles for correct rendering of the styleguide. 
-     return gulp.src('styles/main.less')
+     return gulp.src('styles/**/*.less')
      .pipe(gulpless())
      .pipe(gulpconcat('public/style.css'))
-     .pipe(gulp.dest('styleguide/'));
+     .pipe(gulp.dest('styleguide/'))
+     .pipe(browserSync.stream());
  });
 
 
 
-
+gulp.task('watch', function() {
+ browserSync.init({
+    server: "./styleguide/"
+});
+ gulp.watch('styles/styleguide.md', ['kss']);
+ gulp.watch('styles/*.less', ['render']);
+});
 
 
 gulp.task('default',['kss', 'render'], function() {
--- styles/styleguide.md
+++ styles/styleguide.md
@@ -1,2 +1,6 @@
-
+/*This is Template for creating Living Styleguide*/
 <h1>Scucess</h1>
+<h3>Heading</h3>
+
+
+
